64cab32ab8ff550dda460acf4430fcd49b167e4e
tc-hirata <hirata.naoko.timecapsule@gmail.com>
2024-03-26 14:54:58 +0900

    メモ一覧画面の構造化

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,59 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+    {/* タイトル */}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      <View>
+      {/* メモアイテム */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年3月26日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年3月26日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年3月26日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
