88639ce2b7b14ef93947a00de322a05ea2d82532
tc-hirata <hirata.naoko.timecapsule@gmail.com>
2024-04-10 16:37:19 +0900

    リダイレクトの設定

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,8 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
-import Header from '../components/Header'
-import MemoListItem from '../components/MemoListItem'
-import CircleButton from '../components/CircleButton'
+import { Redirect } from 'expo-router'
 
 const Index = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
-      <CircleButton>+</CircleButton>
-    </View>
-  )
+  return <Redirect href='memo/detail' />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff'
-  }
-})
 
 export default Index
